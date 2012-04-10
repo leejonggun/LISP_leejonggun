e7d2a9d5053135c11308e73ca0d7313efd8215ef
JosephLee <leejonggun@gmail.com>
2012-04-10 14:50:16 +0900

    (maybe)become slimmed-down

--- tokenize.c
+++ tokenize.c
@@ -31,11 +31,11 @@ void tokenize(const char *input) {	//void型はreturn不要
 	int op = 0;	//op means the kind of operator.
 	int tmp = 0;		//文字の初めのjindex
 	int char_token = 0;	//文字数
-	for(jindex = 0; jindex < inputSize; jindex++){
-//	do{
+//	for(jindex = 0; jindex < inputSize; jindex++){
+	do{
 		if(input[jindex] != ' '){
 //Recognition round bracket, Operator
-/*			switch(input[jindex]){
+			switch(input[jindex]){
 				case '(':
 				case ')':
 				case '+':
@@ -46,50 +46,24 @@ void tokenize(const char *input) {	//void型はreturn不要
 					printf("one_symbol_data '%C', tt = %d\n",one_symbol_data,tt);
 					break;
 				default:
-					*/		
-			if(input[jindex] == '('){
-				tt = one_symbol(input, jindex);	//ttの値で条件分けする。ttの値がOPENなら他の関数を呼んで、CLOSEなら文字の読み込み、データに格納を終了する。SYMBOLならその時のinput[op]の値により演算子の区別ができる。
-				printf("one_symbol_data'%c', tt = %d\n",one_symbol_data,tt);
-			}
-			if(input[jindex] == ')'){
-				tt = one_symbol(input, jindex);
-				printf("one_symbol_data'%c', tt = %d\n",one_symbol_data,tt);
-			}
-			if(input[jindex] == '*'){
-				tt = one_symbol(input, jindex);
-				printf("one_symbol_data'%c', tt = %d\n",one_symbol_data,tt);
-			}else if(input[jindex] == '+'){
-				tt = one_symbol(input, jindex);
-				printf("one_symbol_data'%c', tt = %d\n",one_symbol_data,tt);
-			}else if(input[jindex] == '-'){
-				tt = one_symbol(input, jindex);
-				printf("one_symbol_data'%c', tt = %d\n",one_symbol_data,tt);
-			}else if(input[jindex] == '/'){
-				tt = one_symbol(input, jindex);
-				printf("one_symbol_data'%c', tt = %d\n",one_symbol_data,tt);
-			}
-
 //Recognition Number 関数numberを呼び出す。
-			if(47 < input[jindex] && input[jindex] < 58){
-					tt = number(input, jindex);
-printf("log_number num_data = %d, tt = %d\n",num_data,tt);
-				continue;
-//				break;
-			}
+					if(47 < input[jindex] && input[jindex] < 58){
+						tt = number(input, jindex);
+						printf("log_number num_data = %d, tt = %d\n",num_data,tt);
+						break;
+					}
 //Recognition Charactor 関数symbolを呼び出す。
-			if(64 < input[jindex] && input[jindex] < 91 || 96 < input[jindex] && input[jindex] < 123){
-				tt = symbol(input, jindex);
-printf("log_symbol sym_data = %s, tt = %d\n",sym_data,tt);
-				free(sym_data);
-				continue;
-//				break;
+					if(64 < input[jindex] && input[jindex] < 91 || 96 < input[jindex] && input[jindex] < 123){
+						tt = symbol(input, jindex);
+						printf("log_symbol sym_data = %s, tt = %d\n",sym_data,tt);
+						free(sym_data);
+						break;
+					}
 			}
-		
 		}else if(input[jindex] == ' '){
-			printf("");
-//			jindex++;
+			jindex++;
 		}
-	}//while(jindex != inputSize);
+	}while(jindex != inputSize);
 }
 
 enum token_type symbol(const char *string, int last){
@@ -99,34 +73,27 @@ enum token_type symbol(const char *string, int last){
 	}
 	jindex = last;
 	int length = last - first;
-//	char *buf;
-//	buf = (char*)malloc(sizeof(length+1));		//文字数+1分メモリーを確保する。
-	sym_data = (char*)malloc(sizeof(length+1));
-//*buf1 = (char*)malloc(sizeof(char_token+1));じゃないところがわからない。buf1はメモリー配列の最初のアドレス。*buf1はbuf1の指すアドレスの中身
+	sym_data = (char*)malloc(sizeof(length+1));//文字数+1分メモリーを確保する。
 	j = 0;
 	for(i = first; i <= last; i++){
 //	buf[j] = string[i];							//どうして *buf1[j] = input[i]; じゃだめか？
 	sym_data[j] = string[i];		
 	j++;
 	}
-//	buf[j] = '\0';								//文字列の最後は\0です。
 	sym_data[j] = '\0';
-//	printf("log_symbol sym_data = %s\n", sym_data);
-
-//	sym_data = buf;					//文法の間違い。bufの文字列をsym_dataにコピーしたことにならない
-//	free(buf);						//メモリーの開放。
+	jindex++;
 	return SYMBOL;
 }
 
 enum token_type number(const char *string, int first){	//number関数は読み込んだ数字の最初を渡して、enum ttのnumberへ格納する
-	int num_token = string[first]-48;	//数字のトークン
-	enum token_type tt;
+	int num_token = string[first]-48;	//最初の数字
 	while(47 < string[first+1] && string[first+1] < 58){
 		num_token = num_token * 10 + (string[first+1]-48);
 		first++;
 	}
 	jindex = first;			//最後の数字のindexを覚える。
 	num_data = num_token;
+	jindex++;				//次のindexにしてループに返す
 	return NUMBER;
 }
 
@@ -134,23 +101,23 @@ enum token_type one_symbol(const char *string, int index){
 	one_symbol_data = string[index];
 	switch(one_symbol_data){
 		case '(':
-//			jindex++;	//jindexに次のindexを指すようにする。
+			jindex++;	//jindexに次のindexを指すようにする。
 			return OPEN;//文字を続けて読み込む処理を加える。
 			break;
 		case ')':
-//			jindex++;
+			jindex++;
 			return CLOSE;//文字の読み込みを終了する処理を加える。
 			break;
 		case '+':
 		case '*':
 		case '-':
 		case '/':
-//			jindex++;
+			jindex++;
 			return SYMBOL;//文字を続けて読み込む処理を加える。
 			break;
 		default:
 			one_symbol_data = 0;
-//			jindex++;
+			jindex++;
 	printf("error one_symbol_data = %d, token_type = %d\n",one_symbol_data,tt);
 			return SYMBOL;
 			break;
